01e924b034a899f0249fcaf4f951e6a2688781fa
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-07-31 23:12:15 +0200

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let el = document.getElementById("level");
+let level = 0;
+
+while (el) {
+	level++;
+	el = el.parentElement;
+}
+
+ alert("The level of the element is: " + level);
